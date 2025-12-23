93a2bd6372f04983bbb5fb3ec66c0d9364f2ef9c
Edem Veliev <edem010100@gmail.com>
2025-12-23 19:20:22 +0500

    env example

--- src/infrastructure/controllers/dtos/category-budgets/get-dto.ts
+++ src/infrastructure/controllers/dtos/category-budgets/get-dto.ts
@@ -1,11 +1,14 @@
 import { IsInt, Max, Min } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class GetCategoryBudgetsQueryDto {
+    @Type(() => Number)
     @IsInt()
     @Min(1970)
     @Max(3000)
     year: number;
 
+    @Type(() => Number)
     @IsInt()
     @Min(1)
     @Max(12)
